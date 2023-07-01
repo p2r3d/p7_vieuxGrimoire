711b02754ac40e88628df0210a1a13aa9317d4eb
pierrardp <pierrardp@laposte.net>
2023-07-01 10:30:05 +0200

    fix: simplification du code avec try/catch

--- controllers/user.js
+++ controllers/user.js
@@ -6,59 +6,55 @@ const bcrypt = require ('bcrypt');
 const jwt = require ('jsonwebtoken');
 
 // enregistrement des users
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
+  try {
   // On vérifie que l'email n'existe pas dans la BD
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (user) {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
         // email existant
         return res.status(401).json({ message: 'Cet email existe déjà dans la base de données' });
-      } else {
-        // hachage du mdp 
-        bcrypt.hash(req.body.password, 10)
-          .then(hash => {
-            const user = new User({
-              email: req.body.email,
-              password: hash
-            });
-            // Enregistrement dans la base de données
-            user.save()
-              .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-              .catch(error => res.status(400).json({ error }));
-          })
-          .catch(error => res.status(500).json({ error }));
-      }
-    })
-    .catch(error => res.status(500).json({ error }));
-};
+    }
+    // hachage du mdp 
+    const hachedPwd = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      email: req.body.email,
+      password: hachedPwd
+    });
+    // Enregistrement dans la base de données
+    await newUser.save()
+    res.status(201).json({ message: 'Utilisateur créé !' });
+  }
+  catch(error) {
+    res.status(500).json({ error });
+  }
+}
 
 // connexion des utilisateurs existants
-exports.login = (req, res, next) => {
-  // on utilise la méthode findOne de Mogoose pour trouver un user qui possède l'email de la requête
-  User.findOne({ email: req.body.email })
-  .then(user => {
+exports.login = async (req, res, next) => {
+  try {
+    // on utilise la méthode findOne de Mogoose pour trouver un user qui possède l'email de la requête
+    const user = await User.findOne({ email: req.body.email });
     // pas de user trouvé
     if (!user) {
       return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
     }
     // on compare le mdp fourni avec celui haché stocké dans la BD
-    bcrypt.compare(req.body.password, user.password)
-    .then(valid => {
-      // les mdp sont différents
-      if (!valid) {
-        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
-      }
-      // les mdp correspondent 
-      res.status(200).json({
-        userId: user._id,
-        token: jwt.sign( // token généré, contient id user + signé avec clé secrète
-				  {userId:user._id}, // payload = données que l'on veut encoder
-				  process.env.SECRET_KEY,	// clé secrète pour l'encodage
-					{expiresIn: '24h'} // délai
-				)
-      });             
-    })
-    .catch(error => res.status(500).json({ error }));
-  })
-  .catch(error => res.status(500).json({ error }));
-};
+    const valid = await bcrypt.compare(req.body.password, user.password);
+    // si les mdp sont différents
+    if (!valid) {
+      return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+    }
+    // les mdp correspondent 
+    res.status(200).json({
+      userId: user._id,
+      token: jwt.sign( // token généré, contient id user + signé avec clé secrète
+      {userId:user._id}, // payload = données que l'on veut encoder
+      process.env.SECRET_KEY,	// clé secrète pour l'encodage
+      {expiresIn: '24h'} // délai
+      )
+    });             
+  }
+  catch(error) {
+    res.status(500).json({ error });
+  } 
+}
